cc4628da87981d280a474acb2fe478cf12ce2bfc
Thanasis Polychronakis <thanpolas@gmail.com>
2015-11-21 16:52:19 +0200

    work on bootstrap script

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,30 @@
-require('angular');
+/**
+ * BorderShare Learn where to make donations for people in need.
+ * http://bordershare.gr/
+ *
+ * Copyright (c) 2015 SheShar & SKGTech
+ * @author Theodore Kelloglou
+ * @author Thanasis Polychronakis
+ * @author Andigoni Founta
+ *
+ * @fileOverview The Front Application bootstrap file.
+ */
 
-angular.module('app');
+require('angular/angular');
+require('angular-route/angular-route');
+require('angular-bootstrap/ui-bootstrap-tpls');
+
+/**
+ * Master boot of angular app
+ *
+ */
+angular.module('app', [
+  'ngRoute',
+]);
+
+// Templates
+
+// Require components
+
+// Page scripts
+require('./parse');
