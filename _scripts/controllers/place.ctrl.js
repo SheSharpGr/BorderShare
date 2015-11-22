1affe8c2bbd5f473fed16eed2430f767f1af2905
Thanasis Polychronakis <thanpolas@gmail.com>
2015-11-22 15:21:25 +0200

    refactor place controller

--- _scripts/controllers/place.ctrl.js
+++ _scripts/controllers/place.ctrl.js
@@ -1,6 +1,8 @@
 /**
  * @fileOverview The Places controller.
  */
+var Promise = require('bluebird');
+
 require('../services/place.service');
 var helpers = require('../util/helpers');
 
@@ -14,29 +16,104 @@ var PlacesCtrl = module.exports = function($rootScope, $scope, $log, $location,
 
   this.$rootScope = $rootScope;
   this.$log = $log;
+  this.$location = $location;
   this.placesService = placesService;
 
-  /** @type {Array.<Object>} the places model used by the ui */
+  /** @type {Array.<Object>} the places model used by the ui, contains filtered */
   this.places = [];
 
-  this.initData();
+  /** @type {Array.<Object>} the places model used by the app, contains all */
+  this.allPlaces = [];
+
+  /** @type {Array.<string>} The area filter items in slugs */
+  this.filterAreaItems = [];
+  /** @type {Array.<string>} The actual labels to use */
+  this.filterAreaItemLabels = [];
+
+  /** @type {String} The active area filter */
+  this.activeAreaFilter = '';
+
+  this.initData()
+    .bind(this)
+    .then(this.setupFilters)
+    .return($rootScope)
+    .then(helpers.safeApply);
 };
 
 /**
  * Initialize data...
  *
  */
-PlacesCtrl.prototype.initData = function() {
-  var self = this;
-  this.placesService.get()
+PlacesCtrl.prototype.initData = Promise.method(function() {
+  return this.placesService.get()
+    .bind(this)
     .then(function(data) {
-      self.$log.log('app.ctrl.PlacesCtrl() :: Got places:', data.length);
-      self.places = data;
-      helpers.safeApply(self.$rootScope);
+      this.$log.log('app.ctrl.PlacesCtrl() :: Got places:', data.length);
+      this.allPlaces = data;
+      return data;
     });
+});
+
+/**
+ * Setup the available filters.
+ * 
+ * @param {Array.<Object>} places The places.
+ * @return {Promise} A Promise.
+ */
+PlacesCtrl.prototype.setupFilters = Promise.method(function(places) {
+  this._setUniqueAreaItems(places);
+
+  // check if we have area filters in place
+  var query = this.$location.search();
+
+  if (query.area) {
+    this.activeAreaFilter = query.area;
+
+    this.places = places.filter(this._areaFilter, this);
+  } else {
+    this.places = places;
+  }
+});
+
+/**
+ * The area filter callback method.
+ * 
+ * @param {Object} placeItem The place item to examine.
+ * @return {boolean} True / False.
+ */
+PlacesCtrl.prototype._areaFilter = function(placeItem) {
+  return placeItem.bigArea === this.activeAreaFilter;
+};
+
+/**
+ * Get the unique filter area items from the places dataset.
+ * 
+ * @param {Array.<Object>} places The places.
+ * @private
+ */
+PlacesCtrl.prototype._setUniqueAreaItems = function(places) {
+  this.filterAreaItems = ['all'];
+  this.filterAreaItemLabels = ['Όλες'];
+
+  places.forEach(function(placeItem) {
+    console.log('placeItem:', placeItem.bigArea);
+    if (typeof placeItem.bigArea !== 'string') {
+      return;
+    }
+    if (placeItem.bigArea.trim() === '') {
+      return;
+    }
+
+    if (this.filterAreaItems.indexOf(placeItem.bigArea) === -1) {
+      this.filterAreaItems.push(placeItem.bigArea);
+      this.filterAreaItemLabels.push(placeItem.bigAreaLabel);
+    }
+  }, this);
+
 };
 
 angular.module('app')
-  .controller('PlacesCtrl', ['$rootScope', '$scope', '$log', '$location', 'PlacesService',
+  .controller('PlacesCtrl', ['$rootScope', '$scope', '$log', '$location',
+    'PlacesService',
     PlacesCtrl
   ]);
