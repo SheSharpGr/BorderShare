f6973588ec471ce400a8411f8482cab7cca95fad
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-11-22 17:33:33 +0200

    transform icons data on service

--- _scripts/services/place.service.js
+++ _scripts/services/place.service.js
@@ -10,8 +10,9 @@ var Promise = require('bluebird');
  * @param {$http} $http The http service.
  * @constructor
  */
-var PlacesService = module.exports = function ($http) {
+var PlacesService = module.exports = function ($http, $log) {
   this.$http = $http;
+  this.$log = $log;
 };
 
 /**
@@ -23,20 +24,26 @@ PlacesService.prototype.get = Promise.method(function () {
   var Places = Parse.Object.extend('Places');
 
   var query = new Parse.Query(Places);
-  
+  var self = this;
+
   return query.find()
     .then(function(results) {
       var data = [];
       for (var i = 0; i < results.length; i++) {
         var object = results[i];
-        data.push(object.toJSON());
+        var jsonObj = object.toJSON();
+        try {
+          jsonObj.collectingIcons = JSON.parse(jsonObj.collectingIcons);
+        } catch (e) {
+          self.$log.log(e);
+        }
+        data.push(jsonObj);
       }
-
       return data;
     });
 });
 
 angular.module('app')
-  .service('PlacesService', ['$http',
+  .service('PlacesService', ['$http', '$log',
     PlacesService
   ]);
