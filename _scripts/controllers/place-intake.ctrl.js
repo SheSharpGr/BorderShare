ad504fef282d3ef5c14b126beb50059663096c78
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-11-28 18:23:32 +0200

    add place intake controller

---
+++ _scripts/controllers/place-intake.ctrl.js
@@ -0,0 +1,37 @@
+/**
+ * @fileOverview The Place Intake controller.
+ */
+var Promise = require('bluebird');
+// var _ = require('lodash');
+
+/**
+ * The Place Intake controller.
+ *
+ * @constructor
+ */
+var PlaceIntakeCtrl = module.exports = function($log, placesService) {
+
+  $log.log('app.ctrl.PlaceIntakeCtrl.Ctor() :: Init');
+
+  this.placesService = placesService;
+};
+
+/**
+ * Submit new place record.
+ *
+ */
+PlaceIntakeCtrl.prototype.submitNew = Promise.method(function() {
+
+  return this.placesService.newPlace()
+    .bind(this)
+    .then(function() {
+      this.$log.log('app.ctrl.PlaceItemCtrl.submitNew() :: New place record');
+      return;
+    });
+
+});
+
+angular.module('app')
+  .controller('PlaceIntakeCtrl', ['$log', 'PlacesService',
+    PlaceIntakeCtrl
+  ]);
